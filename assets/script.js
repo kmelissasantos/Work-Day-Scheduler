9df79b8f1ac1cd7321d49938a5fa3618c042523d
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-05-16 19:05:51 -0400

    updated time-block text area

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,12 @@ currentDay.text (moment().format('dddd, MMM, Do, YYYY'));
 // Display current hour
 var currentHour = moment().hour();
 
+// Time-Block Text Areas
+$(".time-block").each(function() {
+// value of each hour
+var blockHour = $(this).attr("id");
+// Get value from local storage and display in Time-Block text area
+var textEntry = localStorage.getItem(blockHour);
+var textArea = $(this).find(".description");
+textArea.val (textEntry);
+});
