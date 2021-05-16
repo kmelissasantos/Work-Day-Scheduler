8ee15bab8465323e0277ee68529172ca93bb17a9
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-05-16 18:19:22 -0400

    added current day and current hour in script.js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Display current day in the header 
+var currentDay = $("#currentDay");
+currentDay.text (moment().format('dddd, MMM, Do, YYYY'));
+
+// Display current hour
+var currentHour = moment().format('h:mm:ss a');
+
